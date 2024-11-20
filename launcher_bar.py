3582eb5670dc67b8183ea9c9761aaa365a3bc78f
Ratilife <ratilife81@gmail.com>
2024-11-20 15:45:14 +0300

    Создана стартовая панель launcher_bar.py создан модуль main

---
+++ launcher_bar.py
@@ -0,0 +1,11 @@
+import PySimpleGUI as sg
+
+#панель
+launcher_buttons = {
+                    sg.SYMBOL_DOWN_ARROWHEAD : None,
+                    'Exit': None
+                    }
+
+# с этого метода все начинается
+def start_launcher():
+    pass
---
+++ main.py
@@ -0,0 +1,4 @@
+def main():
+    start_launcher()
+if __name__ == '__main__':
+    main()
