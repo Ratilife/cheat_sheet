2a58b3ad0f64d21b925058eb1f1c7b6d55b07602
Ratilife <ratilife81@gmail.com>
2024-11-25 21:49:54 +0300

    изменен метод create_button()

--- ViewModel/СreateFolderViewModel.py
+++ ViewModel/СreateFolderViewModel.py
@@ -2,10 +2,10 @@ from ViewModel.launcher_bar_ViewModel import launcher_bar_ViewModel
 
 class СreateFolderViewModel:
 
-    def create_button(self, launcher_buttons,name_button,path_folder ):
+    def create_button(self,name_button,path_folder ):
         lbVM = launcher_bar_ViewModel()
-        lb = lbVM.add_button(launcher_buttons, name_button, path_folder)
-        return lb
+        lbVM.add_button(name_button, path_folder)
+
 
 
     def create_bookmark(self,name_button,path_folder):
