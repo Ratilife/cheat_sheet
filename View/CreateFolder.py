20d8ffdb594ff370938dc6aa00f771fd2545b498
Ratilife <ratilife81@gmail.com>
2024-11-25 21:47:36 +0300

    Исправление ошибок

--- View/CreateFolder.py
+++ View/CreateFolder.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 from ViewModel.launcher_bar_ViewModel import launcher_bar_ViewModel
 from ViewModel.СreateFolderViewModel import СreateFolderViewModel
 class СreateFolder:
-    def startCreateFolder(self,determine,launcher_buttons = None):
+    def startCreateFolder(self,determine):
         # determine - определяет текст на форме True - закладки, False - кнопки
         bookmark = 'Введите название закладки'
         button_text = 'Введите название кнопки'
@@ -34,9 +34,8 @@ class СreateFolder:
                     cfVM.create_bookmark(values['-BUTTON_NAME-'],values['-FILE_PATH-'])
                 else:
                     # исполнение в другом модуле
-                    lb = cfVM.create_button(launcher_buttons,values['-BUTTON_NAME-'],values['-FILE_PATH-'])
+                    cfVM.create_button(values['-BUTTON_NAME-'],values['-FILE_PATH-'])
                     break  # Закрыть окно после добавления
         window.close()
-        return lb
 
 
