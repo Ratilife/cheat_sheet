860aaefbfbac85415604f8fc594fb7e974a6f9a1
Ratilife <ratilife81@gmail.com>
2024-11-25 21:49:24 +0300

    Добавлен метод refresh_launcher(), исправлены ошибки

--- ViewModel/launcher_bar_ViewModel.py
+++ ViewModel/launcher_bar_ViewModel.py
@@ -7,19 +7,23 @@ class launcher_bar_ViewModel:
         Перезапускает окно лаунчера, применяя изменения в конфигурации.
         """
         View.launcher_bar.start_launcher_bar()  # Перезапуск основного окна лаунчера
-    def add_button(self,launcher_buttons,key,value):
+
+    def refresh_launcher(self):
+        self.__refresh_launcher()
+    def add_button(self,key,value):
         # Преобразуем словарь в список пар ключ-значение
-        items = list(launcher_buttons.items())
+        items = list(View.launcher_bar.launcher_buttons.items())
         # Определяем индекс для вставки: перед последним элементом
         insert_index = max(0, len(items) - 1)  # Если словарь пуст, индекс будет 0
         # Вставляем новую пару на предпоследнюю позицию
         items.insert(insert_index, (key, value))
         # Преобразуем список обратно в словарь
-        return dict(items)
+        View.launcher_bar.launcher_buttons = dict(items)
 
-    def delete_button(self,launcher_buttons,key):
-        removed_value = launcher_buttons.pop(key, None)
-        return removed_value
+
+    def delete_button(self,key):
+        View.launcher_bar.launcher_buttons = launcher_buttons.pop(key, None)
+        self.__refresh_launcher()
 
     def delete_all_button(self):
         View.launcher_bar.deleteLauncherButton = True
