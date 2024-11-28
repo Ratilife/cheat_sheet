2e6336dcbc1f7310cae62c94d8560c19a2eb5d47
Ratilife <ratilife81@gmail.com>
2024-11-28 15:57:09 +0300

    Удален лишний код

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from View.launcher_bar import start_launcher_bar
 def main():
     start_launcher_bar()
-    #startCreateFolder(False)
-    #start_SettingMenu()
+
 if __name__ == '__main__':
     main()
