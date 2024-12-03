4d67ad26076df8f1e83cced241626e99d7cf9702
Ratilife <ratilife81@gmail.com>
2024-12-03 21:53:57 +0300

    Добавлен метод create_bookmark()

--- ViewModel/СreateFolderViewModel.py
+++ ViewModel/СreateFolderViewModel.py
@@ -1,5 +1,5 @@
 from ViewModel.launcher_bar_ViewModel import launcher_bar_ViewModel
-
+from Model.WorkingWithFolders import WorkingWithFolders
 class СreateFolderViewModel:
 
     def create_button(self,name_button,path_folder ):
@@ -8,5 +8,9 @@ class СreateFolderViewModel:
 
 
 
-    def create_bookmark(self,name_button,path_folder):
+    def create_bookmark(self,name_bookmark,path_folder):
+        # создать папку
+        wwf = WorkingWithFolders()
+        wwf.check_and_create_bookmark(path_folder,name_bookmark)
+        # создать закладку
         pass
